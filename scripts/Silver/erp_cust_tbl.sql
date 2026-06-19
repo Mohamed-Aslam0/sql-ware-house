7b081cb4124feb5c8cf54419e4facdde16bad247
Mohamed Aslam <mohamedaslam2414@gmail.com>
2026-06-19 19:00:24 +0530

    Add INSERT transformation for ERP_CUST_AZ12 to erp_cust_tbl.sql

--- scripts/Silver/erp_cust_tbl.sql
+++ scripts/Silver/erp_cust_tbl.sql
@@ -1,1 +1,17 @@
+-- Insert transformed records from BRONZE.ERP_CUST_AZ12 into silver.erp_cust_az12
+-- Normalizes CID (removes NAS prefix), validates birthdate, and normalizes gender values
 
+INSERT INTO silver.erp_cust_az12(cid, bdate, gen)
+SELECT
+    CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LENGTH(cid))
+         ELSE cid
+    END AS cid,
+    CASE WHEN bdate > CURRENT_DATE THEN NULL ELSE bdate END AS bdate,
+    CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+         WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+         ELSE 'n/a'
+    END AS gen
+FROM
+    BRONZE.ERP_CUST_AZ12
+WHERE
+    CID NOT IN (SELECT CST_key FROM silver.crm_cust_info);

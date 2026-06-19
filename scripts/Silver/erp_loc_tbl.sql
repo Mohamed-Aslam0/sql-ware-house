218b8a28e13f9aca9802a81e0f0afaa05f8a9d19
Mohamed Aslam <mohamedaslam2414@gmail.com>
2026-06-19 19:25:43 +0530

    Add INSERT query to populate silver.erp_loc_a101 from bronze table

--- scripts/Silver/erp_loc_tbl.sql
+++ scripts/Silver/erp_loc_tbl.sql
@@ -1,1 +1,10 @@
-
+INSERT INTO silver.erp_loc_a101(cid,cntry)
+SELECT
+	REPLACE(CID, '-', ''),
+	Case when trim(cntry) = 'DE' then 'Germany'
+	when trim(cntry) in ('US','USA') THEN 'United States' 
+	when trim(cntry) in ('null','') THEN 'n\\a'
+	else cntry
+	end as cntry
+FROM
+	BRONZE.ERP_LOC_A101;
